d9fb5adb1d2b1c0d5a2a9bcefdedf81d64921399
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-01 08:49:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php include("lib/layouts/header.php"); ?>
 <link rel="stylesheet" href="css/app.css">
 
+<div class="container">
+    <a href=""><button class="btn btn primary">Book a Ticket</button></a>
+</div>
+
 <script src="js/script.js"></script>
