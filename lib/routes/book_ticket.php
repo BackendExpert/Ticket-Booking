3522a6f92c524fe2aa4367037e4f22d1ccd5a453
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-03 21:38:41 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/book_ticket.php
+++ lib/routes/book_ticket.php
@@ -10,19 +10,19 @@
             <div class="row">
                 <div class="col-lg-5">
                     <label for="nic">NIC : </label>
-                    <input type="text" name="user_nic" id="" class="form-control" placeholder="NIC Number">
+                    <input type="text" name="user_nic" id="" class="form-control" placeholder="NIC Number" required> 
                 </div>
                 <div class="col-lg-1">&nbsp;</div>
                 <div class="col-lg-6">
                     <label for="eamil">Email : </label>
-                    <input type="email" name="user_email" id="" class="form-control" placeholder="Email">
+                    <input type="email" name="user_email" id="" class="form-control" placeholder="Email" required>
                 </div>
             </div>
             <br>
             <div class="row">
                 <div class="col-lg-12">
                     <label for="surname">Surname</label>
-                    <input type="text" name="surname" id="" class="form-control" placeholder="Surname">
+                    <input type="text" name="surname" id="" class="form-control" placeholder="Surname" required>
                 </div>
             </div>
         </form>
