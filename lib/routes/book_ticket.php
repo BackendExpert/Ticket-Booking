c3d388027ffe37be8fd57247581d71b9cf976675
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-03 21:11:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/book_ticket.php
+++ lib/routes/book_ticket.php
@@ -1,5 +1,10 @@
 <?php include("../layouts/header.php"); ?>
 <link rel="stylesheet" href="../../css/app.css">
 
+<div class="container">
+    <section class="booking-section">
+        <h2>Book A ticket</h2>
+    </section>
+</div>
 
 <script src="../../js/script.js"></script>
