cae4d8bf3085e87a784386ed5d9af9cd5f2a5e7e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-03 21:35:00 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/book_ticket.php
+++ lib/routes/book_ticket.php
@@ -6,8 +6,19 @@
         <h2>Book A ticket</h2>
         <hr>
 
-        
-
+        <form action="<?php echo($_SERVER['PHP_SELF']);?>" method="POST">
+            <div class="row">
+                <div class="col-lg-5">
+                    <label for="nic">NIC : </label>
+                    <input type="text" name="user_nic" id="" class="form-control" placeholder="NIC Number">
+                </div>
+                <div class="col-lg-1">&nbsp;</div>
+                <div class="col-lg-6">
+                    <label for="eamil">Email : </label>
+                    <input type="email" name="user_email" id="" class="form-control" placeholder="Email">
+                </div>
+            </div>
+        </form>
 
 
     </section>
