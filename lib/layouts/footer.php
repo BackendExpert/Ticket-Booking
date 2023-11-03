10350bb5e3ba02682f8f2c1af5554fcb672c8eb0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-03 21:21:24 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/layouts/footer.php
+++ lib/layouts/footer.php
@@ -1,8 +1,9 @@
 <link rel="stylesheet" href="../../css/app.css">
 
-<hr>
-<footer>
-    <b>Developed by</b> <a href="https://github.com/JehanKandy" target="_blank">JehanKandy</a> and <a href="https://github.com/BackendExpert" target="_blank">Jehankandy</a>|| 2021 - 2023
-</footer>
-
+<div class="container">
+    <hr>
+    <footer>
+        <b>Developed by</b> <a href="https://github.com/JehanKandy" target="_blank">JehanKandy</a> and <a href="https://github.com/BackendExpert" target="_blank">Jehankandy</a>|| 2021 - 2023
+    </footer>
+</div>
 <script src="../../js/script.js"></script>
