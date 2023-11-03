a0299ba5144be5fce257786f4a19a5246a2fe38e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-03 21:18:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/layouts/footer.php
+++ lib/layouts/footer.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="../../css/app.css">
 
 <footer>
-    <b>Developed by <a href="" target="_blank">JehanKandy</a> || 2021 - 2023</b>
+    <b>Developed by</b> <a href="" target="_blank">JehanKandy</a> || 2021 - 2023
 </footer>
 
 <script src="../../js/script.js"></script>
