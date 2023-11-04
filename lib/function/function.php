a25dc494c49ea3d772aafc3a2dd39ca0b2f2b7cf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-04 16:32:03 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -5,5 +5,11 @@
 
     function book_ticket($nic, $email, $sn, $fn, $ln){
         $con = Connection();
+
+        echo $nic."<br>";
+        echo $email."<br>";
+        echo $sn."<br>";
+        echo $fn."<br>";
+        echo $ln."<br>";
     }
 ?>
