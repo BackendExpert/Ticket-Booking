73d4d40da1e7ca968e9ac9168ec73060e476ce06
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-05 14:41:00 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/function/function.php
+++ lib/function/function.php
@@ -15,6 +15,6 @@
         $insert_data = "INSERT INTO user_tbl(nic,email,sname,fname,lname)VALUES('$nic','$email','$sn','$fn','$ln')";
         $insert_data_result = mysqli_query($con, $insert_data);
 
-        header("location:../user_data.php");
+        header("location:user_data.php");
     }
 ?>
---
+++ lib/routes/user_data.php
@@ -0,0 +1,8 @@
+<?php include("../layouts/header.php"); ?>
+<?php include("../function/function.php"); ?>
+
+<link rel="stylesheet" href="../../css/app.css">
+
+<?php include("../layouts/footer.php"); ?>
+
+<script src="../../js/script.js"></script>
