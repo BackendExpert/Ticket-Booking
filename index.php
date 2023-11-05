c0df2e288e1f1732e3396c6d762f72816514b94a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-05 14:47:46 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="css/app.css">
 
 <div class="container">
-    <a href="lib/routes/book_ticket.php"><button class="btn btn-primary">Book a Ticket</button></a>
+    <a href="lib/views/login.php"><button class="btn btn-primary">Book a Ticket</button></a>
 </div>
 
 <script src="js/script.js"></script>
