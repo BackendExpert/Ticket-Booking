4f6a17e0c42e34715cb83f59ffbbb3491f7d8886
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-05 14:42:46 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/routes/user_data.php
+++ lib/routes/user_data.php
@@ -3,6 +3,13 @@
 
 <link rel="stylesheet" href="../../css/app.css">
 
+<div class="container">
+    <section class="confarm-section">
+        <h2></h2>
+    </section>
+</div>
+
+
 <?php include("../layouts/footer.php"); ?>
 
 <script src="../../js/script.js"></script>
